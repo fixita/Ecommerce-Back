09ef20596ce6955f78461fbb59e6ab5862e0e32f
SebaVRdev <sebarm177@gmail.com>
2024-10-10 18:12:34 -0300

    fix: Se entrega direccion de comanies y se soluciona respuesta en plural para "products"

--- src/controllers/company.js
+++ src/controllers/company.js
@@ -1,8 +1,15 @@
+import Address from "../models/Address.js";
 import Company from "../models/Company.js";
 
 export const getCompanies = async (req, res) => {
     try {
-        const companies = await Company.findAll();
+        const companies = await Company.findAll({
+          include: [
+            {
+              model: Address
+            }
+          ]
+        });
         res.status(200).json({ ok: true, companies });
     } catch (error) {
         res.status(500).json({ msg: "Error en el servidor" })
--- src/controllers/product.js
+++ src/controllers/product.js
@@ -2,8 +2,8 @@ import Product from "../models/Product.js";
 
 export const getProducts = async (req, res) => {
     try {
-        const product = await Product.findAll();
-        res.status(200).json({ ok: true, product });
+        const products = await Product.findAll();
+        res.status(200).json({ ok: true, products });
     } catch (error) {
         res.status(500).json({ msg: "Error en el servidor" });
     }
